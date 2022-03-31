6a11a4653434aa296b585430a2dbd1f3bb0e5ac5
mlavan1 <mahendranlavan29@gmail.com>
2022-03-31 10:04:10 +0530

    made-a-layout1

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -1,302 +1,169 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <title>Profile</title>
-    <link rel="stylesheet" href="style.css">
-    <!-- Boxicons CDN Link -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="{{ asset('css/dash.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-   <style>
-       .size{
-           font-size:12px;
-       }
-   </style>
-<body>
-  <div class="sidebar">
-    <div class="logo-details">
-      <i class='bx bxl-c-plus-plus'></i>
-      <span class="logo_name">AB Company</span>
+@extends('admin.layout')
+@section('heading')Profile @endsection
+@section('content1')
+<div class="home-content">
+    <div class="overview-boxes">
+      <div class="box">
+        <div class="right-side">
+          <div class="box-topic">Total Order</div>
+          <div class="number">40,876</div>
+          <div class="indicator">
+            <i class='bx bx-up-arrow-alt'></i>
+            <span class="text">Up from yesterday</span>
+          </div>
+        </div>
+        <i class='bx bx-cart-alt cart'></i>
+      </div>
+      <div class="box">
+        <div class="right-side">
+          <div class="box-topic">Total Sales</div>
+          <div class="number">38,876</div>
+          <div class="indicator">
+            <i class='bx bx-up-arrow-alt'></i>
+            <span class="text">Up from yesterday</span>
+          </div>
+        </div>
+        <i class='bx bxs-cart-add cart two' ></i>
+      </div>
+      <div class="box">
+        <div class="right-side">
+          <div class="box-topic">Total Profit</div>
+          <div class="number">$12,876</div>
+          <div class="indicator">
+            <i class='bx bx-up-arrow-alt'></i>
+            <span class="text">Up from yesterday</span>
+          </div>
+        </div>
+        <i class='bx bx-cart cart three' ></i>
+      </div>
+      <div class="box">
+        <div class="right-side">
+          <div class="box-topic">Total Return</div>
+          <div class="number">11,086</div>
+          <div class="indicator">
+            <i class='bx bx-down-arrow-alt down'></i>
+            <span class="text">Down From Today</span>
+          </div>
+        </div>
+        <i class='bx bxs-cart-download cart four' ></i>
+      </div>
     </div>
-      <ul class="nav-links">
-        <li>
-          <a href="#" class="active">
-            <i class='bx bx-grid-alt' ></i>
-            <span class="links_name">Dashboard</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class='bx bx-box' ></i>
-            <span class="links_name">Product</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class='bx bx-list-ul' ></i>
-            <span class="links_name">Order list</span>
-          </a>
-        </li>
-        
-        
-        <li>
-          <a href="#">
-            <i class='bx bx-book-alt' ></i>
-            <span class="links_name">My orders</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class='bx bx-user' ></i>
-            <span class="links_name">Contacts</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class='bx bx-message' ></i>
-            <span class="links_name">Messages</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class='bx bx-heart' ></i>
-            <span class="links_name">Favourites</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class='bx bx-cog' ></i>
-            <span class="links_name">Settings</span>
-          </a>
-        </li>
-        <li class="log_out">
-          <a href="#">
-            <i class='bx bx-log-out'></i>
-            <span class="links_name">Log out</span>
-          </a>
-        </li>
-      </ul>
-  </div>
-  <section class="home-section">
-    <nav>
-      <div class="sidebar-button">
-        <i class='bx bx-menu sidebarBtn'></i>
-        <span class="dashboard">Dashboard</span>
-      </div>
-      <div class="search-box">
-        <input type="text" placeholder="Search...">
-        <i class='bx bx-search' ></i>
-      </div>
-      <div class="profile-details">
-        <!--<img src="images/profile.jpg" alt="">-->
-        <ul class="navbar-nav mr-auto admin_name" >
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Mahendran Lavan {{--({{ $LoginUserInfo['name'] }}) --}}<img src="{{ asset('profile-image.png') }}" alt="" height="25px" width="30px">
-                </a>
-                <div class="dropdown-menu" style="width:150px" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item size" href="javascript:void(0)">Edit profile</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item size" href="javascript:void(0)">Preferences</a>
-                    <a class="dropdown-item size" href="javascript:void(0)">My cart</a>
-                    <a class="dropdown-item size" href="javascript:void(0)">Messages</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item size" href="{{ route('auth.logout') }}" style="color:red">Logout</a>
-                </div>
-                
-                </li>
-            </ul>
-        
-        {{-- <i class='bx bx-chevron-down' ></i> --}}
-      </div>
-    </nav>
 
-    <div class="home-content">
-      <div class="overview-boxes">
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Order</div>
-            <div class="number">40,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bx-cart-alt cart'></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Sales</div>
-            <div class="number">38,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bxs-cart-add cart two' ></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Profit</div>
-            <div class="number">$12,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bx-cart cart three' ></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Return</div>
-            <div class="number">11,086</div>
-            <div class="indicator">
-              <i class='bx bx-down-arrow-alt down'></i>
-              <span class="text">Down From Today</span>
-            </div>
-          </div>
-          <i class='bx bxs-cart-download cart four' ></i>
-        </div>
-      </div>
-
-      <div class="sales-boxes">
-        <div class="recent-sales box">
-          <div class="title">Recent Sales</div>
-          <div class="sales-details">
-            <ul class="details">
-              <li class="topic">Date</li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-              <li><a href="#">02 Jan 2021</a></li>
-            </ul>
-            <ul class="details">
-            <li class="topic">Customer</li>
-            <li><a href="#">Alex Doe</a></li>
-            <li><a href="#">David Mart</a></li>
-            <li><a href="#">Roe Parter</a></li>
-            <li><a href="#">Diana Penty</a></li>
-            <li><a href="#">Martin Paw</a></li>
-            <li><a href="#">Doe Alex</a></li>
-            <li><a href="#">Aiana Lexa</a></li>
-            <li><a href="#">Rexel Mags</a></li>
-             <li><a href="#">Tiana Loths</a></li>
+    <div class="sales-boxes">
+      <div class="recent-sales box">
+        <div class="title">Recent Sales</div>
+        <div class="sales-details">
+          <ul class="details">
+            <li class="topic">Date</li>
+            <li><a href="#">02 Jan 2021</a></li>
+            <li><a href="#">02 Jan 2021</a></li>
+            <li><a href="#">02 Jan 2021</a></li>
+            <li><a href="#">02 Jan 2021</a></li>
+            <li><a href="#">02 Jan 2021</a></li>
+            <li><a href="#">02 Jan 2021</a></li>
+            <li><a href="#">02 Jan 2021</a></li>
           </ul>
           <ul class="details">
-            <li class="topic">Sales</li>
-            <li><a href="#">Delivered</a></li>
-            <li><a href="#">Pending</a></li>
-            <li><a href="#">Returned</a></li>
-            <li><a href="#">Delivered</a></li>
-            <li><a href="#">Pending</a></li>
-            <li><a href="#">Returned</a></li>
-            <li><a href="#">Delivered</a></li>
-             <li><a href="#">Pending</a></li>
-            <li><a href="#">Delivered</a></li>
-          </ul>
-          <ul class="details">
-            <li class="topic">Total</li>
-            <li><a href="#">$204.98</a></li>
-            <li><a href="#">$24.55</a></li>
-            <li><a href="#">$25.88</a></li>
-            <li><a href="#">$170.66</a></li>
-            <li><a href="#">$56.56</a></li>
-            <li><a href="#">$44.95</a></li>
-            <li><a href="#">$67.33</a></li>
-             <li><a href="#">$23.53</a></li>
-             <li><a href="#">$46.52</a></li>
-          </ul>
-          </div>
-          <div class="button">
-            <a href="#">See All</a>
-          </div>
+          <li class="topic">Customer</li>
+          <li><a href="#">Alex Doe</a></li>
+          <li><a href="#">David Mart</a></li>
+          <li><a href="#">Roe Parter</a></li>
+          <li><a href="#">Diana Penty</a></li>
+          <li><a href="#">Martin Paw</a></li>
+          <li><a href="#">Doe Alex</a></li>
+          <li><a href="#">Aiana Lexa</a></li>
+          <li><a href="#">Rexel Mags</a></li>
+           <li><a href="#">Tiana Loths</a></li>
+        </ul>
+        <ul class="details">
+          <li class="topic">Sales</li>
+          <li><a href="#">Delivered</a></li>
+          <li><a href="#">Pending</a></li>
+          <li><a href="#">Returned</a></li>
+          <li><a href="#">Delivered</a></li>
+          <li><a href="#">Pending</a></li>
+          <li><a href="#">Returned</a></li>
+          <li><a href="#">Delivered</a></li>
+           <li><a href="#">Pending</a></li>
+          <li><a href="#">Delivered</a></li>
+        </ul>
+        <ul class="details">
+          <li class="topic">Total</li>
+          <li><a href="#">$204.98</a></li>
+          <li><a href="#">$24.55</a></li>
+          <li><a href="#">$25.88</a></li>
+          <li><a href="#">$170.66</a></li>
+          <li><a href="#">$56.56</a></li>
+          <li><a href="#">$44.95</a></li>
+          <li><a href="#">$67.33</a></li>
+           <li><a href="#">$23.53</a></li>
+           <li><a href="#">$46.52</a></li>
+        </ul>
         </div>
-        <div class="top-sales box">
-          <div class="title">Top Seling Product</div>
-          <ul class="top-sales-details">
-            <li>
-            <a href="#">
-              <!--<img src="images/sunglasses.jpg" alt="">-->
-              <span class="product">Vuitton Sunglasses</span>
-            </a>
-            <span class="price">$1107</span>
-          </li>
+        <div class="button">
+          <a href="#">See All</a>
+        </div>
+      </div>
+      <div class="top-sales box">
+        <div class="title">Top Seling Product</div>
+        <ul class="top-sales-details">
           <li>
-            <a href="#">
-               <!--<img src="images/jeans.jpg" alt="">-->
-              <span class="product">Hourglass Jeans </span>
-            </a>
-            <span class="price">$1567</span>
-          </li>
-          <li>
-            <a href="#">
-             <!-- <img src="images/nike.jpg" alt="">-->
-              <span class="product">Nike Sport Shoe</span>
-            </a>
-            <span class="price">$1234</span>
-          </li>
-          <li>
-            <a href="#">
-              <!--<img src="images/scarves.jpg" alt="">-->
-              <span class="product">Hermes Silk Scarves.</span>
-            </a>
-            <span class="price">$2312</span>
-          </li>
-          <li>
-            <a href="#">
-              <!--<img src="images/blueBag.jpg" alt="">-->
-              <span class="product">Succi Ladies Bag</span>
-            </a>
-            <span class="price">$1456</span>
-          </li>
-          <li>
-            <a href="#">
-              <!--<img src="images/bag.jpg" alt="">-->
-              <span class="product">Gucci Womens's Bags</span>
-            </a>
-            <span class="price">$2345</span>
-          <li>
-            <a href="#">
-              <!--<img src="images/addidas.jpg" alt="">-->
-              <span class="product">Addidas Running Shoe</span>
-            </a>
-            <span class="price">$2345</span>
-          </li>
+          <a href="#">
+            <!--<img src="images/sunglasses.jpg" alt="">-->
+            <span class="product">Vuitton Sunglasses</span>
+          </a>
+          <span class="price">$1107</span>
+        </li>
+        <li>
+          <a href="#">
+             <!--<img src="images/jeans.jpg" alt="">-->
+            <span class="product">Hourglass Jeans </span>
+          </a>
+          <span class="price">$1567</span>
+        </li>
+        <li>
+          <a href="#">
+           <!-- <img src="images/nike.jpg" alt="">-->
+            <span class="product">Nike Sport Shoe</span>
+          </a>
+          <span class="price">$1234</span>
+        </li>
+        <li>
+          <a href="#">
+            <!--<img src="images/scarves.jpg" alt="">-->
+            <span class="product">Hermes Silk Scarves.</span>
+          </a>
+          <span class="price">$2312</span>
+        </li>
+        <li>
+          <a href="#">
+            <!--<img src="images/blueBag.jpg" alt="">-->
+            <span class="product">Succi Ladies Bag</span>
+          </a>
+          <span class="price">$1456</span>
+        </li>
+        <li>
+          <a href="#">
+            <!--<img src="images/bag.jpg" alt="">-->
+            <span class="product">Gucci Womens's Bags</span>
+          </a>
+          <span class="price">$2345</span>
+        <li>
+          <a href="#">
+            <!--<img src="images/addidas.jpg" alt="">-->
+            <span class="product">Addidas Running Shoe</span>
+          </a>
+          <span class="price">$2345</span>
+        </li>
 <li>
-            <a href="#">
-             <!--<img src="images/shirt.jpg" alt="">-->
-              <span class="product">Bilack Wear's Shirt</span>
-            </a>
-            <span class="price">$1245</span>
-          </li>
-          </ul>
-        </div>
+          <a href="#">
+           <!--<img src="images/shirt.jpg" alt="">-->
+            <span class="product">Bilack Wear's Shirt</span>
+          </a>
+          <span class="price">$1245</span>
+        </li>
+        </ul>
       </div>
     </div>
-  </section>
-
-  <script>
-    let sidebar = document.querySelector(".sidebar");
-    let sidebarBtn = document.querySelector(".sidebarBtn");
-    sidebarBtn.onclick = function() {
-      sidebar.classList.toggle("active");
-      if(sidebar.classList.contains("active")){
-        sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
-    }
-    else
-        sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-    }
- </script>
-
-</body>
-</html>
-
+  </div>
+@endsection
